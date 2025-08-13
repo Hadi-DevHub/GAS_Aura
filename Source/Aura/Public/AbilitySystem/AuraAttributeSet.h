04200a6303185155b21ae930f032e5fdc06c5598
HadiDev <Hadidev.business@gmail.com>
2025-08-13 11:48:53 +0700

    25. Creating Health and Mana, they're FGameplayAttributeData not floats, and made the replicated with GAS's way

--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -13,5 +13,38 @@ UCLASS()
 class AURA_API UAuraAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+
+public:
+	UAuraAttributeSet();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital Attribute")
+	FGameplayAttributeData Health;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Vital Attribute")
+	FGameplayAttributeData MaxHealth;
+
+	UFUNCTION()
+	void OnRep_Health(FGameplayAttributeData OldHealth) const;
+
+	UFUNCTION()
+	void OnRep_MaxHealth(FGameplayAttributeData OldMaxHealth) const;
+	
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category = "Vital Attribute")
+	FGameplayAttributeData Mana;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxMana, Category = "Vital Attribute")
+	FGameplayAttributeData MaxMana;
+
+	UFUNCTION()
+	void OnRep_Mana(FGameplayAttributeData OldMana) const;
+
+	UFUNCTION()
+	void OnRep_MaxMana(FGameplayAttributeData OldMaxMana) const;
+protected:
+	
+private:
 	
 };
+
+
