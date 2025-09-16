17acd1208c63da310555a167122a7ba7e1897dff
HadiDev <Hadidev.business@gmail.com>
2025-09-17 05:26:19 +0700

    128. Character Class Info. made a new data asset to init attributes

---
+++ Source/Aura/Public/AbilitySystem/Data/CharacterClassInfo.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CharacterClassInfo.generated.h"
+
+class UGameplayEffect;
+
+UENUM(BlueprintType)
+enum class ECharacterClass : uint8
+{
+	Elementalist,
+	Range,
+	Warrior
+};
+
+USTRUCT(BlueprintType)
+struct FCharacterClassDefaultInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, Category = "Class Default")
+	TSubclassOf<UGameplayEffect> PrimaryAttributes;
+	
+};
+
+UCLASS()
+class AURA_API UCharacterClassInfo : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditDefaultsOnly, Category = "Shared Class Default")
+	TMap<ECharacterClass, FCharacterClassDefaultInfo> CharacterClassInformation;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Shared Class Default")
+	TSubclassOf<UGameplayEffect> SecondaryAttributes;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Shared Class Default")
+	TSubclassOf<UGameplayEffect> VitalAttributes;
+
+	FCharacterClassDefaultInfo GetCharacterClass(ECharacterClass CharacterClass) const;
+	
+};
