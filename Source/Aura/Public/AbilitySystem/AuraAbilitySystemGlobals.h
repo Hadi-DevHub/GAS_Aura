d2a515811365ccca5e52cd1cb574b7d3507bfe98
HadiDev <Hadidev.business@gmail.com>
2025-09-29 10:03:30 +0700

    154. Aura Ability System Globals. made a new AbilitySystemGlobals class, and set the script in DefaultGame.ini to make the engine use the custom AuraAbilitySystemGlobals instead, this is needed to make and use the AuraGEContext instead of the default GEContext

---
+++ Source/Aura/Public/AbilitySystem/AuraAbilitySystemGlobals.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemGlobals.h"
+#include "AuraAbilitySystemGlobals.generated.h"
+
+UCLASS()
+class AURA_API UAuraAbilitySystemGlobals : public UAbilitySystemGlobals
+{
+	GENERATED_BODY()
+
+	// harus di override karena pengen menggunakan custom FAuraGameplayEffectContext derived from FGameplayEffectContext
+	virtual FGameplayEffectContext* AllocGameplayEffectContext() const override;
+};
