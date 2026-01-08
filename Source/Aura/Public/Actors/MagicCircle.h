eed8b0df2aa3de3dfbbb1af7c9971a7673126822
HadiDev <Hadidev.business@gmail.com>
2026-01-08 11:13:27 +0700

    345. Magic Circle

--- Source/Aura/Public/Actors/MagicCircle.h
+++ Source/Aura/Public/Actors/MagicCircle.h
@@ -6,21 +6,21 @@
 #include "GameFramework/Actor.h"
 #include "MagicCircle.generated.h"
 
+class UDecalComponent;
+
 UCLASS()
 class AURA_API AMagicCircle : public AActor
 {
 	GENERATED_BODY()
 	
 public:	
-	// Sets default values for this actor's properties
 	AMagicCircle();
-
-protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
+protected:
 	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UDecalComponent> MagicCircleDecal;
 
 };
